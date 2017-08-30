31a3ff722d8ba5603b17b8440166a6fb0d6420f5
Cameron Earl <developer.cameron@gmail.com>
2017-08-29 19:30:56 -0700

    Complete exercise

--- abstract-functions.js
+++ abstract-functions.js
@@ -1,34 +1,21 @@
 // Clean up the code below with a single function called `personalIntroduction`
 // The new code should have the exact same functionality and output.
 
-var name = "Bob";
-var height = "72";
-var favoriteColor = "red";
-var string = "My name is " + name;
-string += ", and my height is " + height;
-string += ", and my favorite color is " + favoriteColor;
-console.log(string);
+const createPerson = (name, height, favoriteColor) {
+  return {"name":name, "height":height, "favoriteColor":favoriteColor};
+}
 
-var name = "Samantha";
-var height = "65";
-var favoriteColor = "orange";
-var string = "My name is " + name;
-string += ", and my height is " + height;
-string += ", and my favorite color is " + favoriteColor;
-console.log(string);
+const bob = createPerson("Bob","72","red");
+const samantha = createPerson("Samantha","65","orange");
+const christopher = createPerson("Christopher","69","green");
+const tamera = createPerson("Tamera","63","blue");
 
-var name = "Christopher";
-var height = "69";
-var favoriteColor = "green";
-var string = "My name is " + name;
-string += ", and my height is " + height;
-string += ", and my favorite color is " + favoriteColor;
-console.log(string);
 
-var name = "Tamera";
-var height = "63";
-var favoriteColor = "blue";
-var string = "My name is " + name;
-string += ", and my height is " + height;
-string += ", and my favorite color is " + favoriteColor;
-console.log(string);
+const personalIntroduction = (p) => {
+  return `My name is ${p.name}, and my height is ${p.height}, and my favorite color is ${p.favoriteColor}`;
+}
+
+console.log(personalIntroduction(bob));
+console.log(personalIntroduction(samantha));
+console.log(personalIntroduction(christopher));
+console.log(personalIntroduction(tamera));
